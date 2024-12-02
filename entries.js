d8c9c45f6cde2e2c8ecedc7de7d5ddf86dd4c14e
dwbert <64101806+dwbert@users.noreply.github.com>
2024-12-02 15:39:51 -0600

    Update entries.js

--- entries.js
+++ entries.js
@@ -1,188 +1,232 @@
 const context = {
 	entries: [
-		{
-			name: 'Hallway Rug',
-			category: 'home',
-			image: 'https://secure.img1-fg.wfcdn.com/im/19406364/resize-h800-w800%5Ecompr-r85/4026/40266290/Broomhedge+Oriental+Brown%2FRust+Area+Rug.jpg',
-			description: 'Broomhedge Oriental Brown/Rust Area Rug',
-			price_label: '$$$',
+    {
+			name: 'Computer Monitor Light',
+			category: 'Office',
+			image: 'https://m.media-amazon.com/images/I/51TH4WwyzhL._AC_SX679_.jpg',
+			description: 'Quntis Computer Monitor Light | Auto-Dimming, Adjustable Brightness and Temperature',
+			price_label: '$$',
 			links: [
 				{
-					price: '$115.99',
-					url: 'https://www.wayfair.com/rugs/pdp/charlton-home-broomhedge-oriental-brownrust-area-rug-w005759826.html?piid=1480287156'
-				}
-			],
-			purchase_ready: false,
-			fund_raising: false,
-			taken: true
-		},
-		{
-			name: 'Wrist Watch',
-			category: 'clothing',
-			image: 'https://m.media-amazon.com/images/I/81Q-Rf3am2L._AC_UY879_.jpg',
-			description: 'SEIKO 5 Automatic Stainless Steel Watch with White Dial - SNK789',
-			price_label: '$$$',
-			links: [
-				{
-					price: '$92.00',
-					url: 'https://www.amazon.com/Seiko-SNK789-Automatic-Stainless-Steel/dp/B0018N0HL0'
-				},
-				{
-					price: '$99.00',
-					url: 'https://www.jomashop.com/seiko-watch-snk789.html'
+					price: '$48.99',
+					url: 'https://www.amazon.com/dp/B08HMLKS2N'
 				}
 			],
 			purchase_ready: true,
 			fund_raising: false,
 			taken: false
 		},
-		{
-			name: 'Watch Band',
-			category: 'clothing',
-			image: 'https://cdn.shopify.com/s/files/1/0835/8495/products/Caramel_Tan_SS_buckle_040249e6-65c8-41f7-9774-b99f9a83aa31_1024x1024@2x.jpg?v=1623255864',
-			description: 'Weathered Brown Leather Quick Release | 18mm | Stainless Steel Buckle',
+    {
+			name: 'Ultrasonic Cleaning Pod',
+			category: 'Hygiene',
+			image: 'https://m.media-amazon.com/images/I/71ah09iE7nL._SX679_.jpg',
+			description: 'Ultrasonic Cleaning Pod | Retainer Cleaner | White',
 			price_label: '$',
 			links: [
 				{
-					price: '$26.00',
-					url: 'https://www.bartonwatchbands.com/collections/leather-watch-bands/products/weathered-brown-leather?variant=31616731906102'
+					price: '$37.59',
+					url: 'https://www.amazon.com/dp/B0DCBMDQ8T'
 				}
 			],
 			purchase_ready: true,
 			fund_raising: false,
 			taken: false
 		},
-		{
-			name: 'Nintendo Switch Travel Case',
-			category: 'video-games',
-			image: 'https://target.scene7.com/is/image/Target/GUEST_12fff90e-fbe8-4e3a-a645-9f3b50c24ee0?fmt=pjpeg&wid=1400&qlt=80',
-			description: 'Nintendo Switch Game Traveler Slim Traveler Case',
+    {
+			name: 'Vacuum Cleaner',
+			category: 'Home',
+			image: 'https://target.scene7.com/is/image/Target/GUEST_82d398bd-c97e-408d-aae4-f7cdbfeeea4a?wid=1200&hei=1200&qlt=80&fmt=pjpeg',
+			description: 'Shark Stratos Upright Vacuum | Model: AZ3002',
+			price_label: '$$$',
+			links: [
+				{
+					price: '$249.99',
+					url: 'https://www.target.com/p/shark-stratos-upright-vacuum-with-duoclean-powerfins-hairpro-self-cleaning-brushroll-odor-neutralizer-technology-navy-az3002/-/A-86794826'
+				},
+        {
+					price: '$279.95',
+					url: 'https://www.amazon.com/dp/B0C87H7QKS'
+				}
+			],
+			purchase_ready: true,
+			fund_raising: true,
+			taken: false
+		},
+    {
+			name: 'XBox Game Pass',
+			category: 'Games',
+			image: 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fi.pinimg.com%2Foriginals%2Fe0%2Fbe%2F50%2Fe0be5066ace675eb18bb130d95be81b9.png&f=1&nofb=1&ipt=82d642d1ae166980cfd1da1c5326b86106e8420b894f3637edf8afc1f346c323&ipo=images',
+			description: 'XBox Game Pass | 3 Months',
+			price_label: '$',
+			links: [
+				{
+					price: '$30.00',
+					url: 'https://www.microsoft.com/en-us/p/xbox-gift-card-digital-code/cfq7ttc0k63h?icid=CNav_Xbox_Gift_Card&activetab=pivot:overviewtab'
+				}
+			],
+			purchase_ready: true,
+			fund_raising: false,
+			taken: false
+		},
+    {
+			name: 'StoveGuard',
+			category: 'Home',
+			image: 'https://m.media-amazon.com/images/I/71oHWPpPVWL._AC_SX679_.jpg',
+			description: 'StoveGuard | Model: KSGG700ESS1 | Black',
+			price_label: '$$',
+			links: [
+				{
+					price: '64.99',
+					url: 'https://www.amazon.com/dp/B07NVV2G6R'
+				}
+			],
+			purchase_ready: true,
+			fund_raising: false,
+			taken: false
+		},
+    {
+			name: 'Orbital Buffer',
+			category: 'Auto',
+			image: 'https://www.harborfreight.com/media/catalog/product/cache/9fc4a8332f9638515cd199dd0f9238da/5/6/56722_W3.jpg',
+			description: '20V Cordless 10 In. Buffer – Tool Only',
+			price_label: '$',
+			links: [
+				{
+					price: '$24.99',
+					url: 'https://www.harborfreight.com/20v-cordless-10-in-buffer-tool-only-56722.html'
+				}
+			],
+			purchase_ready: true,
+			fund_raising: false,
+			taken: false
+		},
+    {
+			name: 'Polishing Bonnets',
+			category: 'Auto',
+			image: 'https://www.harborfreight.com/media/catalog/product/cache/c5a7cc0816faf52ed720d0b3840ada0e/5/9/59772_W3.jpg',
+			description: '10 in. Terry Microfiber and Fleece Polishing Bonnets, 4-Pack',
+			price_label: '$',
+			links: [
+				{
+					price: '$6.99',
+					url: 'https://www.harborfreight.com/10-in-terry-microfiber-and-fleece-polishing-bonnets-4-pack-59772.html'
+				}
+			],
+			purchase_ready: true,
+			fund_raising: false,
+			taken: false
+		},
+    {
+			name: 'Handhelp Jigsaw',
+			category: 'Home',
+			image: 'https://www.harborfreight.com/media/catalog/product/cache/9fc4a8332f9638515cd199dd0f9238da/6/3/63630_W3.jpg',
+			description: '20V Cordless Variable Speed Jig Saw - Tool Only',
+			price_label: '$$',
+			links: [
+				{
+					price: '$29.97',
+					url: 'https://www.harborfreight.com/power-tools/power-saws/jig-saws/20v-cordless-variable-speed-jig-saw-tool-only-63630.html'
+				}
+			],
+			purchase_ready: true,
+			fund_raising: false,
+			taken: false
+		},
+    {
+			name: 'Watch Band',
+			category: 'Fashion',
+			image: 'http://www.bartonwatchbands.com/cdn/shop/files/V2CaramelTanSSBuckle_1fdaa494-38fb-4543-aa59-4287bcf8f759.jpg?v=1686849160',
+			description: 'Weathered Brown Leather Quick Release watch band | 18mm | Silver Buckle',
+			price_label: '$',
+			links: [
+				{
+					price: '$30.99',
+					url: 'https://www.bartonwatchbands.com/products/weathered-brown-leather'
+				}
+			],
+			purchase_ready: true,
+			fund_raising: false,
+			taken: false
+		},
+    {
+			name: 'Watch Band',
+			category: 'Fashion',
+			image: 'http://www.bartonwatchbands.com/cdn/shop/products/Black_Brighter_767de9cf-0cbe-4d1b-9e06-660be36c642c.jpg?v=1645414167',
+			description: 'Black Alligator Leather Quick Release watch band | 20mm | Silver Buckle',
+			price_label: '$',
+			links: [
+				{
+					price: '$35.99',
+					url: 'https://www.bartonwatchbands.com/products/black-alligator-grain-leather?variant=19225391202358'
+				}
+			],
+			purchase_ready: true,
+			fund_raising: false,
+			taken: false
+		},
+    {
+			name: 'IR Remote Extender',
+			category: 'Tech',
+			image: 'https://m.media-amazon.com/images/I/41BLwPZBNmL._AC_.jpg',
+			description: '1.5m IR Infrared Emitter Extender Cable Extension',
+			price_label: '$',
+			links: [
+				{
+					price: '$6.99',
+					url: 'https://www.amazon.com/dp/B08C7X7T5M'
+				}
+			],
+			purchase_ready: true,
+			fund_raising: false,
+			taken: false
+		},
+    {
+			name: 'Phone Case',
+			category: 'Tech',
+			image: 'https://m.media-amazon.com/images/I/716mBXWOjeL._AC_SX679_.jpg',
+			description: 'Case for iPhone 7 Plus/8 Plus Case with Slide Camera Cover',
 			price_label: '$',
 			links: [
 				{
 					price: '$12.99',
-					url: 'https://www.target.com/p/nintendo-switch-game-traveler-slim-traveler-case/-/A-76439793#lnk=sametab',
-				}		
-			],
-			purchase_ready: true,
-			fund_raising: false,
-			taken: false
-		},
-		{
-			name: 'Bike Lights',
-			category: 'bike',
-			image: 'https://m.media-amazon.com/images/I/71CcHj5j2-S._AC_SX679_.jpg',
-			description: 'Vont "Pyro" Bike Light Set',
-			price_label: '$',
-			links: [
-				{
-					price: '$11.89',
-					url: 'https://www.amazon.com/Vont-Rechargeable-Illumination-Resistant-Accessories/dp/B078TB9TMK'
+					url: 'https://www.amazon.com/dp/B0D1K1VD4P'
 				}
 			],
 			purchase_ready: true,
 			fund_raising: false,
 			taken: false
 		},
-		{
-			name: 'Bridge',
-			category: 'pool',
-			image: 'https://media.pooldawg.com/media/catalog/product/cache/1/image/9df78eab33525d08d6e5fb8d27136e95/b/h/bhjb_01.jpg',
-			description: 'Justa Bridge Billiards Bridge',
-			price_label: '$$',
-			links: [
-				{
-					price: '$36.95',
-					url: 'https://www.pooldawg.com/justa-bridge-billiards-bridge',
-				}		
-			],
-			purchase_ready: false,
-			fund_raising: false,
-			taken: true
-		},
-		{
-			name: 'Jump/Break Cue',
-			category: 'pool',
-			image: 'https://m.media-amazon.com/images/I/418gj5N-ozL._AC_.jpg',
-			description: 'RAGE Heavy Hitter Jump Break Cue - 25oz 3 Piece Pool Stick with Extra Hard bakelite tip',
-			price_label: '$$$',
-			links: [
-				{
-					price: '$97.01',
-					url: 'https://www.amazon.com/25oz-Rage-Heavy-Hitter-Break/dp/B00EAIF6SY'
-				}
-			],
-			purchase_ready: false,
-			fund_raising: false,
-			taken: true
-		},
-		{
-			name: 'Super Mario Odyssey',
-			category: 'video-games',
-			image: 'https://m.media-amazon.com/images/I/51NgDXzErYL.jpg',
-			description: 'Super Mario Odyssey | Nintendo Switch | Standard | Physical Game',
+    {
+			name: 'Shoe Organizer',
+			category: 'Home',
+			image: 'https://m.media-amazon.com/images/I/71bf3j8KgVL._AC_SX679_.jpg',
+			description: 'Plastic Shoe Storage Organizer Unit | Grey',
 			price_label: '$$',
 			links: [
 				{
 					price: '$38.99',
-					url: 'https://www.gamestop.com/video-games/nintendo-switch/products/super-mario-odyssey/10141908.html?condition=New'
+					url: 'https://www.amazon.com/dp/B085SWT6B8'
 				}
 			],
 			purchase_ready: true,
 			fund_raising: false,
 			taken: false
 		},
-		{
-			name: 'Purell Dispenser',
-			category: 'home',
-			image: 'https://m.media-amazon.com/images/I/71Q3m075gOL._SL1500_.jpg',
-			description: 'Purell ES1 Hand Sanitizer Dispenser',
-			price_label: '$',
-			links: [
-				{
-					price: '$15.99',
-					url: 'https://www.amazon.com/Sanitizer-Dispenser-Starter-Push-Style-Advanced/dp/B08NFJ7TG5'
-				}
-			],
-			purchase_ready: true,
-			fund_raising: false,
-			taken: false
-		},
-		{
-			name: 'Birkenstocks',
-			category: 'clothing',
-			image: 'https://www.birkenstock.com/dw/image/v2/BDXC_PRD/on/demandware.static/-/Sites-master-catalog/default/dw4d46e7e4/951313/951313.jpg?sw=848&sh=848',
-			description: 'Arizona Soft Footbed | Mocha | Size 10',
+    {
+			name: 'Metal Filing Cabinet',
+			category: 'Office',
+			image: 'https://m.media-amazon.com/images/I/61FLJdqM3XL.__AC_SX300_SY300_QL70_ML2_.jpg',
+			description: 'BIZOEIRON 3 Drawer File Cabinet',
 			price_label: '$$$',
 			links: [
 				{
-					price: '$125.00',
-					url: 'https://www.amazon.com/Birkenstock-Arizona-Birko-Flo-Birko-Flor-Sandals/dp/B000EYBKSI'
-				},
-				{
-					price: '$135.00',
-					url: 'https://www.birkenstock.com/us/arizona-suede-leather/arizona-core-suedeleather-softfootbed-eva-u_90.html?dwvar_arizona-core-suedeleather-softfootbed-eva-u__90_size=260&region=US'
-				}
-			],
-			purchase_ready: false,
-			fund_raising: false,
-			taken: true
-		},
-		{
-			name: 'Bicycle Tires',
-			category: 'bike',
-			image: 'https://m.media-amazon.com/images/I/412CG7MPSZL._AC_.jpg',
-			description: 'Set of 2, Sunlite Road Raised Center Tire - 27 x 1-1/4, Black/Gum',
-			price_label: '$$',
-			links: [
-				{
-					price: '$62.95',
-					url: 'https://www.amazon.com/Sunlite-Road-Raised-Center-Tire/dp/B00BMFJ7ZQ'
+					price: '119.99',
+					url: 'https://www.amazon.com/dp/B0C9HLK921'
 				}
 			],
 			purchase_ready: true,
 			fund_raising: false,
 			taken: false
-		}
-	]
+		},
+  ]
 };
